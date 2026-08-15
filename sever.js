7ca141bedfc7af9f8e48785f71c119262aefda87
legendarydogmathr908768-ctrl <legendarydogmathr908768@gmail.com>
2026-08-15 16:49:34 +0630

    Add files via upload

---
+++ sever.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
+
+app.use(express.static('public'));
+
+io.on('connection', (socket) => {
+    socket.on('join-room', (roomId) => {
+        socket.join(roomId);
+        console.log(`User joined: ${roomId}`);
+    });
+
+    socket.on('roll', (data) => {
+        io.to(data.roomId).emit('opponent-rolled', data.value);
+    });
+});
+
+const PORT = process.env.PORT || 3000;
+http.listen(PORT, () => console.log('Server running on port ' + PORT));
